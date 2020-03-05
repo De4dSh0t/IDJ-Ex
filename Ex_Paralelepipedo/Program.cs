4dc7144265fd28790b9185fa2456fc01e65629f4
De4dSh0t <ruipsilva2001@hotmail.com>
2020-03-05 15:16:12 +0000

    Paralelepipedo v1.1

---
+++ Ex_Paralelepipedo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex_Paralelepipedo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Paralelepipedo p = new Paralelepipedo(r.Next(-10, 100), r.Next(-10, 100), r.Next(-10,100));
+
+            Console.WriteLine("");
+            Console.WriteLine("////////////////////////////////////////////////////");
+            Console.WriteLine($"Volume: {p.Volume()}");
+            Console.WriteLine($"Área Total: {p.Area()}");
+            Console.WriteLine($"É 3D? {p.Is3D()}");
+            Console.WriteLine($"É um cubo? {p.IsACube()}");
+            Console.WriteLine($"Dimensões: {p.ToString()}");
+            Console.WriteLine("////////////////////////////////////////////////////");
+            Console.WriteLine("");
+        }
+    }
+}
