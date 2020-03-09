0c11256a793c8f4e876b8eef497963784813d204
De4dSh0t <ruipsilva2001@hotmail.com>
2020-03-09 15:53:32 +0000

    AlunoISMAI v1.0

---
+++ Ex_AlunoISMAI/Ex_AlunoISMAI/Aluno.cs
@@ -0,0 +1,58 @@
+﻿namespace Ex_AlunoISMAI
+{
+    public class Aluno
+    {
+        private string nome;
+        private int numero;
+        private int ano;
+        private string curso;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        public int Ano
+        {
+            get { return ano; }
+            set { ano = value; }
+        }
+
+        public string Curso
+        {
+            get { return curso; }
+            set { curso = value; }
+        }
+
+        public Aluno(string nome, int numero, int ano, string curso)
+        {
+            Nome = nome;
+            Numero = numero;
+            Ano = ano;
+            Curso = curso;
+        }
+
+        public override string ToString()
+        {
+            return $"{nome.ToString()} {numero.ToString()}, {ano.ToString()}º {curso.ToString()}.";
+        }
+
+        public bool InscritoAntes(Aluno a)
+        {
+            if (a.numero < numero) { return true; }
+            else { return false; }
+        }
+
+        public string PrimeiroUltimoNome()
+        {
+            return "0";
+        }
+    }
+}
---
+++ Ex_AlunoISMAI/Ex_AlunoISMAI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex_AlunoISMAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno("Rui Silva", 36062, 1, "DJD");
+            Aluno b = new Aluno("Fábio Almeida", 36438, 1, "DJD");
+
+            Console.WriteLine("");
+            Console.WriteLine("/////////////////////////////////////////////");
+            Console.WriteLine($"{a.ToString()}");
+            Console.WriteLine($"Foi inscrito antes de {b.Nome}? {a.InscritoAntes(b)}");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine($"{b.ToString()}");
+            Console.WriteLine($"Foi inscrito antes de {a.Nome}? {b.InscritoAntes(a)}");
+            Console.WriteLine("/////////////////////////////////////////////");
+            Console.WriteLine("");
+        }
+    }
+}
