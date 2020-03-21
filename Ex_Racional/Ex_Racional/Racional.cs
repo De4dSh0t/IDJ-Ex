c6d660d8fe728ba385251c13f857c2dabf9c9303
De4dSh0t <ruipsilva2001@hotmail.com>
2020-03-21 16:35:13 +0000

    Racional v0.1 (Added Properties and Constructor)

---
+++ Ex_Racional/Ex_Racional/Racional.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ex_Racional
+{
+    public class Racional
+    {
+        private int numerator;
+        private int denominator;
+
+        public int Numerator
+        {
+            set
+            {
+                if (value.GetType() != typeof(int)) //Caso o user introduza um valor não inteiro...
+                {
+                    throw new InvalidOperationException("Introduza um valor inteiro!");
+                }
+
+                numerator = value;
+            }
+        }
+
+        public int Denominator
+        {
+            set
+            {
+                if (value.GetType() != typeof(int)) //Caso o user introduza um valor não inteiro...
+                {
+                    throw new InvalidOperationException("Introduza um valor inteiro!");
+                }
+
+                denominator = value;
+            }
+        }
+
+        public Racional()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+
+        public Racional(int n, int d)
+        {
+            Numerator = n;
+            Denominator = d;
+        }
+    }
+}
