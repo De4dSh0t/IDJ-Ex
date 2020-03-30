d7fa8d63b8e96994d8d010062a7e20a0ecf96608
De4dSh0t <ruipsilva2001@hotmail.com>
2020-03-30 16:33:44 +0100

    TempConverter v1.0

---
+++ Ex_TemperatureConverter/Ex_TemperatureConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex_TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] words = input.Split( " ");
+                if (words[1] == "Celsius")
+                {
+                    double n = Double.Parse(words[0]);
+                    Console.WriteLine($"{Math.Round(TempConverter.ToFahrenheit(n), 2)} Fahrenheit");
+                }
+                else if(words[1] == "Fahrenheit")
+                {
+                    double n = Double.Parse(words[0]);
+                    Console.WriteLine($"{Math.Round(TempConverter.ToCelsius(n), 2)} Celsius");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Ex_TemperatureConverter/Ex_TemperatureConverter/TempConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex_TemperatureConverter
+{
+    public static class TempConverter
+    {
+        public static double ToFahrenheit(double celsius)
+        {
+            return celsius * 9/5 + 32;
+        }
+
+        public static double ToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5/9;
+        }
+    }
+}
