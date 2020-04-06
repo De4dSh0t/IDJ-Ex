b56feac0a01cefd9967c652b2b2c10c6697ba5fc
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-06 20:06:04 +0100

    PlanckConstant v1.0

---
+++ Ex_PlanckConstant/Ex_PlanckConstant/Calculation.cs
@@ -0,0 +1,13 @@
+﻿namespace Ex_PlanckConstant
+{
+    public class Calculation
+    {
+        private static double plackConstant = 6.62606896e-34;
+        private static double pi = 3.14159;
+
+        public static double ReducedPlanck()
+        {
+            return plackConstant / (2 * pi);
+        }
+    }
+}
---
+++ Ex_PlanckConstant/Ex_PlanckConstant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex_PlanckConstant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculation.ReducedPlanck());
+        }
+    }
+}
