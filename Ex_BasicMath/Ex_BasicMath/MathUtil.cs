db30dd7c07487a2d823b8adcd730f0e179650f4a
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-06 20:21:32 +0100

    BasicMath v1.0

---
+++ Ex_BasicMath/Ex_BasicMath/MathUtil.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ex_BasicMath
+{
+    public class MathUtil
+    {
+        public static float Sum(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        public static float Subtract(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+
+        public static float Multiply(float n1, float n2)
+        {
+            return n1 * n2;
+        }
+
+        public static float Divide(float dividend, float divisor)
+        {
+            return dividend/ divisor;
+        }
+
+        public static float Percentage(float n, float percent)
+        {
+            return percent/100 * n;
+        }
+    }
+}
---
+++ Ex_BasicMath/Ex_BasicMath/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Ex_BasicMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            while (input != "End")
+            {
+                string[] words = input.Split(" ");
+                float n1 = float.Parse(words[1]);
+                float n2 = float.Parse(words[2]);
+                
+                switch (words[0])
+                {
+                    case "Sum":
+                    {
+                        Console.WriteLine($"Result: {MathUtil.Sum(n1, n2)}");
+                        break;
+                    }
+
+                    case "Subtract":
+                    {
+                        Console.WriteLine($"Result: {MathUtil.Subtract(n1, n2)}");
+                        break;
+                    }
+
+                    case "Multiply":
+                    {
+                        Console.WriteLine($"Result: {MathUtil.Multiply(n1, n2)}");
+                        break;
+                    }
+
+                    case "Divide":
+                    {
+                        Console.WriteLine($"Result: {MathUtil.Divide(n1, n2)}");
+                        break;
+                    }
+
+                    case "Percentage":
+                    {
+                        Console.WriteLine($"Result: {MathUtil.Percentage(n1, n2)}");
+                        break;
+                    }
+
+                    default:
+                    {
+                        Console.WriteLine("Please insert 'Sum', 'Subtract', 'Multiply', 'Divide' or 'Percentage'!");
+                        break;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
