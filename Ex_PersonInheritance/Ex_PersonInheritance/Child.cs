72e71901f7e03820b51b602e5284167584f59cd9
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-15 12:56:12 +0100

    Person v1.0

---
+++ Ex_PersonInheritance/Ex_PersonInheritance/Child.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex_PersonInheritance
+{
+    public class Child : Person
+    {
+        public override int Age
+        {
+            get { return base.Age; }
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("Child's age must be less than 15!");
+                }
+                
+                base.Age = value;
+            }
+        }
+
+        public Child(string name, int age) : base(name, age)
+        {
+            Age = age;
+            base.Name = name;
+            base.Age = Age;
+        }
+    }
+}
---
+++ Ex_PersonInheritance/Ex_PersonInheritance/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace Ex_PersonInheritance
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public virtual string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Name's length should not be less than 3 symbols!");
+                }
+                name = value;
+            }
+        }
+        public virtual int Age 
+        {
+            get { return age; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Please, insert a valid age.");
+                }
+
+                age = value;
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"Name: {name}, Age: {age}");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
