ad221c1d708563d9ce650dae306125e8a76ffb7d
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-15 15:10:06 +0100

    BookShop v0.1 (Added "Book" class)

---
+++ Ex_BookShop_Inheritance/Ex_BookShop_Inheritance/Book.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+
+namespace Ex_BookShop_Inheritance
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private double price;
+
+        protected string Title
+        {
+            get => title;
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+
+                title = value;
+            }
+        }
+
+        protected string Author
+        {
+            get => author;
+            set
+            {
+                string[] words = value.Split();
+
+                if (Char.IsLetter(words[1][0]) == false)
+                {
+                    throw new ArgumentException("Author not valid!");
+                }
+
+                author = value;
+            }
+        }
+
+        protected double Price
+        {
+            get => price;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price not valid!");
+                }
+                
+                price = Math.Round(value, 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sB = new StringBuilder();
+            sB.Append("Type: ").Append(GetType().Name)
+                .Append(Environment.NewLine)
+                .Append("Title: ").Append(Title)
+                .Append(Environment.NewLine)
+                .Append("Author: ").Append(Author)
+                .Append(Environment.NewLine)
+                .Append("Pice: ").Append(Price)
+                .Append(Environment.NewLine);
+
+            return sB.ToString();
+        }
+    }
+}
