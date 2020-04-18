de11929475c72e27a8446b8e57e3770e7f393e1a
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-18 19:21:48 +0100

    Mordors v0.1 (Added abstract class "FoodFactory")

---
+++ Ex_MordorsCrueltyPlan_Inheritance/Ex_MordorsCrueltyPlan_Inheritance/FoodFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex_MordorsCrueltyPlan_Inheritance
+{
+    public abstract class FoodFactory
+    {
+        private string name;
+        private int hp;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int HP
+        {
+            get => hp;
+            set => hp = value;
+        }
+    }
+}
---
+++ Ex_MordorsCrueltyPlan_Inheritance/Ex_MordorsCrueltyPlan_Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex_MordorsCrueltyPlan_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
