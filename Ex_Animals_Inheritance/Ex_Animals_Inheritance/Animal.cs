19ce908d40946d49e4a7e6bda516aaed6436af26
De4dSh0t <ruipsilva2001@hotmail.com>
2020-04-19 17:14:40 +0100

    AnimalInheritance v0.1 (Added class "Animal")

---
+++ Ex_Animals_Inheritance/Ex_Animals_Inheritance/Animal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Ex_Animals_Inheritance
+{
+    public class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (age < 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get => gender;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                if (value != "Male" || value != "Female")
+                {
+                    throw new ArgumentException("Invali Input!");
+                }
+
+                gender = value;
+            }
+        }
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+    }
+}
---
+++ Ex_Animals_Inheritance/Ex_Animals_Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex_Animals_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
